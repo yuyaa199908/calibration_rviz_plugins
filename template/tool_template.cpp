193b011d22873e38aa2c28291eda5dbb2dd1ceca
yuyaa199908 <yuyaa199908@gmail.com>
2025-04-25 14:55:25 +0900

    first commit

---
+++ template/tool_template.cpp
@@ -0,0 +1,35 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rviz_common/tool.hpp>
+
+#include "tool_template.hpp"
+
+namespace sample_rviz_plugins
+{
+
+ToolTemplate::ToolTemplate()
+    : rviz_common::Tool()
+{
+}
+
+ToolTemplate::~ToolTemplate()
+{
+}
+
+void ToolTemplate::onInitialize()
+{
+    // nh_ = context_->getRosNodeAbstraction().lock()->get_raw_node();
+    
+}
+
+void ToolTemplate::activate()
+{
+}
+
+void ToolTemplate::deactivate()
+{
+}
+
+}
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS( sample_rviz_plugins::ToolTemplate, rviz_common::Tool )
---
+++ template/tool_template.hpp
@@ -0,0 +1,25 @@
+#ifndef SAMPLE_RVIZ_PLUGINS_TOOL_TEMPLATE_HPP
+#define SAMPLE_RVIZ_PLUGINS_TOOL_TEMPLATE_HPP
+
+#include <rviz_common/tool.hpp>
+
+namespace sample_rviz_plugins
+{
+
+class ToolTemplate : public rviz_common::Tool
+{
+public:
+    ToolTemplate();
+    virtual ~ToolTemplate();
+    virtual void onInitialize();
+    virtual void activate();
+    virtual void deactivate();
+
+protected:
+    // rclcpp::Node::SharedPtr nh_;
+    
+private:
+};
+
+}
+#endif // SAMPLE_RVIZ_PLUGINS_TOOL_TEMPLATE_HPP
